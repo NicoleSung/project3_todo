0ccc66e9f9bb1cf3fa78276389f5cc2e0d669dd7
Osvaldo Hernandez <osvaldo23hs@gmail.com>
2025-04-27 01:38:35 -0400

    Updated PrivateRoute.tsx.

--- client/src/components/auth/PrivateRoute.tsx
+++ client/src/components/auth/PrivateRoute.tsx
@@ -1,5 +1,43 @@
+// import React, { useEffect, useState } from 'react';
+// import { Navigate } from 'react-router-dom';
+
+// interface Props {
+//   children: JSX.Element;
+// }
+
+// const PrivateRoute = ({ children }: Props) => {
+//   const [authChecked, setAuthChecked] = useState(false);
+//   const [authenticated, setAuthenticated] = useState(false);
+
+//   useEffect(() => {
+//     fetch('/api/auth/me', {
+//       method: 'GET',
+//       credentials: 'include',
+//     })
+//       .then(res => {
+//         if (res.ok) {
+//           setAuthenticated(true);
+//         } else {
+//           setAuthenticated(false);
+//         }
+//         setAuthChecked(true);
+//       })
+//       .catch(() => {
+//         setAuthenticated(false);
+//         setAuthChecked(true);
+//       });
+//   }, []);
+
+//   if (!authChecked) return <div>Loading...</div>;
+//   return authenticated ? children : <Navigate to="/login" replace />;
+// };
+
+// export default PrivateRoute;
+
+
 import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
+import { apiFetch } from '../../utils/api';
 
 interface Props {
   children: JSX.Element;
@@ -10,20 +48,23 @@ const PrivateRoute = ({ children }: Props) => {
   const [authenticated, setAuthenticated] = useState(false);
 
   useEffect(() => {
-    fetch('/api/auth/me', {
-      method: 'GET',
-      credentials: 'include',
-    })
+    apiFetch('/api/auth/me')
       .then(res => {
-        if (res.ok) {
-          setAuthenticated(true);
-        } else {
+        if (!res.ok) {
           setAuthenticated(false);
+          return null;
         }
-        setAuthChecked(true);
+        return res.json();
+      })
+      .then(data => {
+        if (data && data.authenticated) {
+          setAuthenticated(true);
+        }
       })
       .catch(() => {
         setAuthenticated(false);
+      })
+      .finally(() => {
         setAuthChecked(true);
       });
   }, []);
